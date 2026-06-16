ce3fb4856874445aa035ddc9da9c18b1a2f63db6
Upkarkaur02 <ukaur60_be24@thapar.edu>
2026-06-16 11:37:54 +0530

    Delete aa.py

--- aa.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-movie_features = pd.read_csv("processed_data/movie_features.csv")
-
-print(movie_features.columns[:130].tolist())
-print("\nTotal columns:", len(movie_features.columns))
